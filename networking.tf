b7be6ba8db151b9724019270f1a4318436a4fed7
Corban Cloud <corban.cloud@yodlee.com>
2021-01-26 16:44:25 -0800

    Adds ingress rule

--- networking.tf
+++ networking.tf
@@ -7,6 +7,14 @@ resource "aws_security_group" "allow_outgoing" {
   description = "Allows out non-existant lambda to make calls out but not recieve any incoming connections"
   vpc_id      = aws_vpc.interview.id
 
+  ingress {
+    description = "Allow All Incoming"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
